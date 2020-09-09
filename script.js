b05a36624062368662f41a17fbcea74ee464cf7a
Josephine Simar <sijo20be@student.ju.se>
2020-09-09 11:58:00 +0200

    Raderade onödiga rader

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 const correctName = "test"; //konstanter för korrekt namn och lösen
 const correctPassword = "1234";
+var content = document.getElementById("content");
 
 //Kallar på startfunktionerna i rätt ordning
 landingPage();
 lsCheck();
 
 function landingPage() { //funktion som skapar startsida med inloggningsformulär
-    document.getElementById("content").innerHTML=""; //innehållet i diven "content" töms för att sedan fyllas på på nytt
+    content.innerHTML=""; //innehållet i diven "content" töms för att sedan fyllas på på nytt
     var inpName = document.createElement("input"); //textfält och en knapp skapas
     var inpPassword = document.createElement("input");
     var btnLogin = document.createElement("button");
     inpName.setAttribute("type", "text"); //inputfälten och knappen tilldelas attribut
-    inpName.setAttribute("value", "");
     inpName.setAttribute("id", "inpName");
     inpName.setAttribute("placeholder", "Namn");
-    inpPassword.setAttribute("type", "password");    
-    inpPassword.setAttribute("value", "");
-    inpPassword.setAttribute("id", "inpPassword");
+    inpPassword.setAttribute("type", "password");
+    //inpPassword.setAttribute("id", "inpPassword");
     inpPassword.setAttribute("placeholder", "Lösenord");
     btnLogin.setAttribute("id", "btnLogin");
     btnLogin.innerText = "Logga in";
     btnLogin.onclick = () => login(); //vid klick på knappen kallas funktionen "login"
-    document.getElementById("content").appendChild(inpName);
-    document.getElementById("content").appendChild(inpPassword);
-    document.getElementById("content").appendChild(btnLogin);
+    content.appendChild(inpName);
+    content.appendChild(inpPassword);
+    content.appendChild(btnLogin);
 }
 
-function lsCheck() { //funktion som körs när startsidan laddar som kollar inlogg i localStorage, om inlogget är rätt skickas användaren direkt till välkomstsidan
+function lsCheck() { //funktion som körs när startsidan laddar som kollar inlogg i localStorage, om det sparade inlogget är rätt skickas användaren direkt till välkomstsidan
     console.log("check");
     if (localStorage.name===correctName && localStorage.password===correctPassword){
         welcomePage();
@@ -37,26 +36,26 @@ function welcomePage() { //funktion som visar välkomstsidan
     console.log("välkommen");
     document.getElementById("content").innerHTML=""; //innehållet i diven "content" töms för att sedan fyllas på på nytt
     var rubrik=document.createElement("h2");
-    var btnLogout=document.createElement("button"); //logga ut-knapp som leder tillbaka till startsidan skapas
+    var btnLogout=document.createElement("button"); //logga ut-knapp som leder tillbaka till startsidan
     rubrik.setAttribute("id", "rubrik");
     rubrik.innerText="Välkommen in!";
     document.getElementById("content").appendChild(rubrik);
-    btnLogout.innerHTML="Logga ut";
+    btnLogout.innerText="Logga ut";
     btnLogout.onclick = () => logout();
-    document.getElementById("content").appendChild(btnLogout);
+    content.appendChild(btnLogout);
 }
 
 function wrongPage() { //funktion som visar felinloggsidan
     console.log("felsida");
     document.getElementById("content").innerHTML=""; //innehållet i diven "content" töms för att sedan fyllas på på nytt
     var rubrik=document.createElement("h2");
-    var btnLogout=document.createElement("button");
+    var btnTryAgain=document.createElement("button");
     rubrik.setAttribute("id", "rubrik");
-    rubrik.innerText="Tyvärr skrev du in fel inloggningsuppgifter, du är inte välkommen in på sidan.";
+    rubrik.innerText="Tyvärr skrev du in fel inloggningsuppgifter, försök gärna igen.";
     document.getElementById("content").appendChild(rubrik);
-    btnLogout.innerHTML="Försök igen"; //knapp som leder tillbaka till startsidan så ett nytt inloggningsförsök kan göras
-    btnLogout.onclick = () => landingPage();
-    document.getElementById("content").appendChild(btnLogout);
+    btnTryAgain.innerHTML="Försök igen"; //knapp som leder tillbaka till startsidan så ett nytt inloggningsförsök kan göras
+    btnTryAgain.onclick = () => landingPage();
+    content.appendChild(btnTryAgain);
 }
 
 function login() { //funktion som testar namn+lösen
